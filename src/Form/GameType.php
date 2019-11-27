0c63e0fe12cd626eb74ff1a783ccc8b48f2bdeb9
skyalderson <julien-tessier-tsgri@orange.fr>
2019-11-27 14:08:13 +0100

    Base projet v2

---
+++ src/Form/GameType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Fight;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('result')
+            ->add('turns')
+            ->add('user')
+            ->add('boss')
+            ->add('stuffs')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Fight::class,
+        ]);
+    }
+}
